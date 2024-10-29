1cb88185760008883c010662fe9789af4d07497c
Eduardo <eduardo.sales.silva1603@gmail.com>
2024-10-29 14:16:40 -0300

    first section

---
+++ refazer/scripts.js
@@ -0,0 +1,17 @@
+/* ----- TYPING EFFECT ----- */
+var typingEffect = new Typed(".typedText",{
+    strings : ["Desenvolvedor", "Front-end", "Back-end", "Fullstack"],
+    loop : true,
+    typeSpeed : 100, 
+    backSpeed : 80,
+    backDelay : 2000
+  })
+  
+  
+  /* ----- ## -- SCROLL REVEAL ANIMATION -- ## ----- */
+  const sr = ScrollReveal({
+        origin: 'top',
+        distance: '80px',
+        duration: 2000,
+        reset: true     
+  })
